e3032b038a40e5d4e752674265814964fdca22d0
Nacho Canon <nachete.canon@gmail.com>
2018-08-17 12:51:39 +0200

    all tests done with exception handler included

---
+++ src/main/java/com/example/gradle/p1/controller/PrimesController.java
@@ -0,0 +1,40 @@
+package com.example.gradle.p1.controller;
+
+import com.example.gradle.p1.exceptions.PrimeNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PrimesController {
+
+    private final PrimesFluxHandler primesFluxHandler;
+
+    @ExceptionHandler(PrimeNotFoundException.class)
+    public ResponseEntity<String> handleNotFoundPrimeException(final PrimeNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @Autowired
+    public PrimesController(final PrimesFluxHandler primesFluxHandler) {
+        this.primesFluxHandler = primesFluxHandler;
+    }
+
+    @Validated
+    @GetMapping(path = "/primesunder/{number}")
+    public Mono<Long> getClosestPrimeUnder(@PathVariable Long number) {
+        return primesFluxHandler.getClosestPrimeUnder(number);
+    }
+
+    @Validated
+    @GetMapping(path = "/isprime/{number}")
+    public Mono<Boolean> isPrime(@PathVariable Long number) {
+        return primesFluxHandler.isPrime(number);
+    }
+}
--- src/main/java/com/example/gradle/p1/service/PrimesService.java
+++ src/main/java/com/example/gradle/p1/service/PrimesService.java
@@ -1,6 +1,5 @@
 package com.example.gradle.p1.service;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.Arrays;
@@ -12,7 +11,6 @@ public class PrimesService {
 
     public static final Set<Long> WELL_KNOWN_FIRST_PRIMES = new TreeSet<>(Arrays.asList(2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L, 29L, 31L, 37L, 41L, 43L, 47L, 53L, 59L, 61L, 67L, 71L, 73L, 79L, 83L, 89L, 97L, 101L, 103L, 107L, 109L, 113L, 127L, 131L, 137L, 139L, 149L, 151L, 157L, 163L, 167L, 173L, 179L, 181L, 191L, 193L, 197L, 199L));
 
-    @Valid
     public Optional<Long> closestPrimeUnder(final @NotNull @Min(0) Long param) {
         final Long from = (param - 1) % 2 == 0 ? param - 2 : param - 1;
         for (long val = from; val > 1; val -= 2) {
@@ -23,7 +21,6 @@ public class PrimesService {
         return Optional.empty();
     }
 
-    @Valid
     public boolean isPrime(final @NotNull @Min(0) Long param) {
         if (param == 1L || param == 0L) {
             return false;
