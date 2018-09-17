f4ddeda5733242ffbce123cb7448eedf52d6cefa
nacho.canon <nachete.canon@gmail.com>
2018-09-17 18:30:10 +0200

    updated but not working yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,109 +1,84 @@
-group = "com.valverde.nacho"
-version = "1.0.0.RELEASE"
+import org.gradle.internal.jacoco.rules.JacocoViolationRuleImpl
 
 buildscript {
     repositories {
         jcenter()
-
     }
     dependencies {
         classpath("io.spring.gradle:dependency-management-plugin:1.0.6.RELEASE")
     }
-}
 
+}
 
 
 subprojects {
-plugins {
-apply(plugin = "java")
-apply(plugin = "pmd")
-apply(plugin = "checkstyle")
-apply(plugin = "jacoco")
-apply(plugin = "io.spring.dependency-management")
-}
+    plugins {
+        apply(plugin = "java")
+        apply(plugin = "pmd")
+        apply(plugin = "checkstyle")
+        apply(plugin = "jacoco")
+        apply(plugin = "io.spring.dependency-management")
+    }
 
-repositories {
-    mavenCentral()
-    listOf("https://repo.spring.io/libs-release",
-"https://repo.spring.io/libs-snapshot").forEach {
-maven {
-url = uri(it)
-}
-}
-}
-
-dependencyManagement {
-imports {
-mavenBom 'io.spring.platform:platform-bom:Cairo-SR3'
-}
-}
-
-sourceCompatibility = '1.10'
-targetCompatibility = '1.10'
+    repositories {
+        mavenCentral()
+        listOf("https://repo.spring.io/libs-release",
+                "https://repo.spring.io/libs-snapshot").forEach {
+            maven {
+                url = uri(it)
+            }
+        }
+    }
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_1_10
+        targetCompatibility = sourceCompatibility
+    }
 
 
-checkstyleMain {
-this.version = "8.11"
-ignoreFailures = false
-configFile = rootProject.file("config/java/checkstyle/checkstyle.xml")
-}
-checkstyleTest {
-this.version = "8.11"
-ignoreFailures = false
-configFile = rootProject.file("config/java/checkstyle/checkstyle.xml")
-}
+    configure<CheckstyleExtension> {
+        toolVersion = "8.11"
+        maxErrors = 0
+        configFile = rootProject.file("config/java/checkstyle/checkstyle.xml")
+    }
+    configure<JacocoPluginExtension> {
+        toolVersion = "0.8.1"
+        evaluationDependsOn("jacocoTestReport")
 
-jacoco {
-toolVersion = "0.8.1"
-}
-
-jacocoTestCoverageVerification {
-dependsOn(jacocoTestReport)
-violationRules {
-failOnViolation = true
-rule {
-enabled = true
-element = 'CLASS'
-excludes = ['*Exception', '*Application']
-
-limit {
-counter = 'LINE'
-value = 'COVEREDRATIO'
-minimum = 0.95
-}
-limit {
-counter = 'BRANCH'
-value = 'COVEREDRATIO'
-minimum = 0.70
-}
-}
-}
-}
-jacocoTestReport {
-reports {
-xml.enabled = false
-html.enabled = true
-}
-}
-
-pmdMain {
-this.version = '4.10'
-ignoreFailures = false
-ruleSetConfig = rootProject.resources.text.fromFile("config/java/pmd/ruleset.xml")
-reports {
-xml.enabled = false
-html.enabled = true
-}
-}
-
-check.dependsOn(checkstyleMain, pmdMain, jacocoTestCoverageVerification)
-configure(subprojects.findAll()) {
-test {
-onlyIf {
-!(project.name = ~ / . * -test.* /)
-}
-}
-}
+    }
+    configure<JacocoReport> {
+        reports {
+            html
+        }
+    }
+    configure<JacocoCoverageVerification> {
+        dependsOn("JacocoReport")
+        violationRules {
+            isFailOnViolation = true
+            rule {
+                enabled = true
+                element = "CLASS"
+                excludes = listOf("*Exception", "*Application")
+                limit {
+                    counter = "LINE"
+                    value = "COVEREDRATIO"
+                    minimum = BigDecimal.valueOf(0.95)
+                }
+                limit {
+                    counter = "BRANCH"
+                    value = "COVEREDRATIO"
+                    minimum = BigDecimal.valueOf(0.70)
+                }
+            }
+        }
+    }
+    configure<PmdExtension> {
+        toolVersion = "4.10"
+        isIgnoreFailures = false
+        ruleSetConfig = rootProject.resources.text.fromFile("config/java/pmd/ruleset.xml")
+    }
+    configure<PmdReports> {
+        html
+    }
 
 
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,23 @@
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath("io.spring.gradle:dependency-management-plugin:1.0.6.RELEASE")
+    }
+
+}
+
 plugins {
     "kotlin-dsl"
 }
-var springBootVersion: String by extra
-springBootVersion= "2.0.4.RELEASE"
-var projectVersion:String by extra
-projectVersion="$version"
+
+object Versioning {
+    val springBootVersion="2.0.4.RELEASE"
+    val groupId="com.valverde.nacho"
+    val projectVersion="1.0.1-SNAPSHOT"
+}
+object Config {
+    object Versioning
+}
